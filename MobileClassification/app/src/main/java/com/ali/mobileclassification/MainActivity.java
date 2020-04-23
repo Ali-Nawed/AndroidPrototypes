00e173afbf38b83947f23424ce689a707998cf18
Ali-Nawed <ali.nawed@mail.utoronto.ca>
2020-04-23 10:12:08 -0400

    Added ML model and predictions output

--- MobileClassification/app/src/main/java/com/ali/mobileclassification/MainActivity.java
+++ MobileClassification/app/src/main/java/com/ali/mobileclassification/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ali.mobileclassification;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.camera2.Camera2Config;
 import androidx.camera.core.Camera;
@@ -16,9 +17,15 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
@@ -26,7 +33,19 @@ import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import org.pytorch.IValue;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -37,12 +56,16 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
 
     private static final int REQUEST_CODE_PERMISSIONS = 10;
     private static final String[] REQUIRED_PERMISSIONS = new String[] {Manifest.permission.CAMERA};
+    private Context context = this;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
     private PreviewView previewView;
     private TextView textView1;
     private TextView textView2;
     private TextView textView3;
+
+    private Tensor inputTensor;
+    private Module module;
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private Executor executor = Executors.newSingleThreadExecutor();
@@ -56,20 +79,11 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
     private float label1Prob;
     private float label2Prob;
     private float label3Prob;
-
+    private FloatBuffer mInputTensorBuffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (allPermissionsGranted()) {
-            startCamera();
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
-        }
-    }
-
-    private void startCamera() {
         previewView = findViewById(R.id.preview_view);
         surfaceView = findViewById(R.id.surfaceView);
         surfaceView.setZOrderMediaOverlay(true);
@@ -80,6 +94,23 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
         textView2 = findViewById(R.id.textView2);
 
         textView3 = findViewById(R.id.textView3);
+
+        try {
+            module = Module.load(getModule(this, "mobilenet_imagenet.pt"));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading module", e);
+            finish();
+            e.printStackTrace();
+        }
+
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    private void startCamera() {
 
 
 
@@ -102,22 +133,60 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
         Preview preview = new Preview.Builder().build();
         ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
                                           .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                          .setTargetResolution(new Size(224,224))
                                           .build();
 
+
+
+
         imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+            @SuppressLint("UnsafeExperimentalUsageError")
+            @WorkerThread
             @Override
             public void analyze(@NonNull ImageProxy image) {
+                try {
+                    if (module == null) {
+                        module = Module.load(getModule(context, "mobilenet_imagenet.pt"));
+                    }
 
+                    FloatBuffer mInputTensorBuffer = Tensor.allocateFloatBuffer(3 * 224 * 224);
+                    Tensor inputTensor = Tensor.fromBlob(mInputTensorBuffer, new long[]{1L, 3L, 224L, 224L});
+
+
+
+                    TensorImageUtils.imageYUV420CenterCropToFloatBuffer(image.getImage(), 0, 224, 224,
+                                                                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB,
+                                                                        TensorImageUtils.TORCHVISION_NORM_STD_RGB,
+                                                                        mInputTensorBuffer,0);
+                    Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+                    final float[] scores = outputTensor.getDataAsFloatArray();
+
+                    String topLabel = "";
+                    float topScore = -Float.MAX_VALUE;
+                    for (int i=0; i<scores.length; i++) {
+                        if (scores[i] > topScore) {
+
+                            topScore =scores[i];
+                            topLabel = ImageNetLabels.labels[i];
+                        }
+                    }
+
+                    image.close();
+                    Log.v("ImageAnalysis", String.format("%s: %s", topLabel, topScore));
+                } catch (IOException e) {
+                    Log.e("Image Analysis", "Error during image analysis");
+                    e.printStackTrace();
+                    image.close();
+                }
             }
         });
-
         CameraSelector cameraSelector = new CameraSelector.Builder()
-                                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                                            .build();
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
 
         Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalysis);
-        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
 
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
 
     }
 
@@ -135,6 +204,25 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
 
     private boolean allPermissionsGranted() {
         return ContextCompat.checkSelfPermission(this, REQUIRED_PERMISSIONS[0]) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static String getModule(Context context, String moduleFileName) throws IOException {
+        File file = new File(context.getFilesDir(), moduleFileName);
+        if (file.exists() && file.length()>0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream inputStream = context.getAssets().open(moduleFileName)) {
+            try (OutputStream outputStream = new FileOutputStream(file)) {
+                byte[] moduleBuffer = new byte[4 * 1024];
+                int read;
+                while ((read=inputStream.read(moduleBuffer)) != -1) {
+                    outputStream.write(moduleBuffer, 0, read);
+                }
+                outputStream.flush();
+            }
+            return file.getAbsolutePath();
+        }
     }
 
     @NonNull
